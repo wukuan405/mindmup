11564281ca4064fb54a881787056a94bf070e53f
davedf <dave@deflorinier.me.uk>
2013-04-07 12:32:42 +0100

    configure proxyload and s3 configurls for offline build

--- web.rb
+++ web.rb
@@ -22,6 +22,8 @@ configure do
   set :key_id_generator, UUID.new
   set :current_map_data_version, ENV['CURRENT_MAP_DATA_VERSION'] || "a1"
   set :network_timeout_millis, ENV['NETWORK_TIMEOUT_MILLIS']||10000
+  set :publishing_config_url, '/publishingConfig'
+  set :proxy_load_url, '/s3proxy/'
   offline =  ENV['OFFLINE'] || "online"
   set :online, offline == "offline" ? false : true
   AWS.config(:access_key_id=>settings.s3_key_id, :secret_access_key=>settings.s3_secret_key)
