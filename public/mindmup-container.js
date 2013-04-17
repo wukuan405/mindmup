dd93ab981607719f7fa2a8e95d92e14000614bdd
Gojko Adzic <gojko@gojko.com>
2013-04-18 00:00:13 +0100

    moved chrome app container to offline app

--- public/mindmup-container.js
+++ public/mindmup-container.js
@@ -1,4 +1,4 @@
-/*global MM, window, jQuery, chrome*/
+/*global MM, window, jQuery*/
 MM.BrowserContainer = function () {
 	'use strict';
 	var self = this;
@@ -27,45 +27,5 @@ MM.BrowserContainer = function () {
 		element.addClass(store[key]);
 		return this;
 	};
-	return self;
-};
-
-MM.ChromeAppContainer = function () {
-	'use strict';
-	var self = this,
-		storage = chrome && chrome.storage && chrome.storage.local;
-	self.bindUnloadEvent = function () {
-	};
-	self.classCachingWidget = function (element, keyPrefix, store) {
-		var key = keyPrefix + '-' + element.selector, observer;
-			store = store || self.storage;
-			observer = new MutationObserver(function(mutations) {
-				store.setItem(key,  element.attr('class'));
-			});
-		$.each(element, function () {
-			observer.observe(this, { attributes: true, childList: false, characterData: false });
-		});
-		store.getItem(key).done(function(result) {
-			element.attr('class', result);
-		});
-		return this;
-	};
-	this.storage = {
-			removeItem: function (key) {
-				storage.remove(key);
-			},
-			setItem: function (key, value) {
-				var val = {};
-				val[key] = value;
-				storage.set(val);
-			},
-			getItem: function (key) {
-				var deferred = jQuery.Deferred();
-				storage.get(key, function (result) {
-					deferred.resolve(result[key]);
-				});
-				return deferred.promise();
-			}
-		};
 	return self;
 };
