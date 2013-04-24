dd854ed7aa868c84bec10e2020210c6e97e6ef01
Gojko Adzic <gojko@gojko.com>
2013-04-24 13:27:45 +0100

    auto save dialog

--- public/auto-save.js
+++ public/auto-save.js
@@ -3,8 +3,8 @@ MM.AutoSave = function (mapRepository, storage) {
 	'use strict';
 	var prefix = 'auto-save-',
 		self = this,
-    currentMapId,
-    currentIdea,
+		currentMapId,
+		currentIdea,
 		checkForLocalChanges = function (mapId) {
 			storage.getItem(prefix + mapId).done(function (value) {
 				if (value) {
@@ -13,29 +13,28 @@ MM.AutoSave = function (mapRepository, storage) {
 			});
 		},
 		trackChanges = function (idea, mapId) {
-      var events = [];
+			var events = [];
 			idea.addEventListener('changed', function (command, params) {
 				events.push({cmd: command, args: params});
 				storage.setItem(prefix + mapId, events);
 			});
 		};
 	observable(this);
-  
-  self.applyUnsavedChanges = function () {
-    storage.getItem(prefix + currentMapId).done(function (events) {
-      if (events) {
-        events.forEach(function (event) {
-          currentIdea[event.cmd].apply(currentIdea, event.args); 
-        });
-      }
+	self.applyUnsavedChanges = function () {
+		storage.getItem(prefix + currentMapId).done(function (events) {
+			if (events) {
+				events.forEach(function (event) {
+					currentIdea[event.cmd].apply(currentIdea, event.args);
+				});
+			}
 		});
-  };
-  self.discardUnsavedChanges = function () {
-    storage.remove(prefix + currentMapId);
-  };
+	};
+	self.discardUnsavedChanges = function () {
+		storage.remove(prefix + currentMapId);
+	};
 	mapRepository.addEventListener('mapLoaded', function (idea, mapId) {
-    currentMapId = mapId;
-    currentIdea = idea;
+		currentMapId = mapId;
+		currentIdea = idea;
 		checkForLocalChanges(mapId);
 		trackChanges(idea, mapId);
 	});
--- test/auto-save-spec.js
+++ test/auto-save-spec.js
@@ -25,66 +25,74 @@ describe('Auto save', function () {
 
 		expect(unsavedChangesAvailableListener).not.toHaveBeenCalled();
 	});
-	it('should cache all change events to storage until map is saved', function () {
-		var changeCmd = 'updateTitle',
-			changeArgs = [1, 'new'];
-		mapRepository.dispatchEvent('mapLoaded', idea, 'mapId');
-		spyOn(storage, 'setItem');
+	describe('event caching', function () {
+		it('should cache all change events to storage until map is saved', function () {
+			var changeCmd = 'updateTitle',
+				changeArgs = [1, 'new'];
+			mapRepository.dispatchEvent('mapLoaded', idea, 'mapId');
+			spyOn(storage, 'setItem');
 
-		idea.dispatchEvent('changed', changeCmd, changeArgs);
+			idea.dispatchEvent('changed', changeCmd, changeArgs);
 
-		expect(storage.setItem).toHaveBeenCalledWith('auto-save-mapId', [ {cmd: 'updateTitle', args: [1, 'new']} ]);
+			expect(storage.setItem).toHaveBeenCalledWith('auto-save-mapId', [ {cmd: 'updateTitle', args: [1, 'new']} ]);
+		});
+		it('should append multiple change events to storage', function () {
+			mapRepository.dispatchEvent('mapLoaded', idea, 'mapId');
+			spyOn(storage, 'setItem');
+
+			idea.dispatchEvent('changed', 'cmd1', [1]);
+			idea.dispatchEvent('changed', 'cmd2', [2]);
+
+			expect(storage.setItem).toHaveBeenCalledWith('auto-save-mapId', [ {cmd: 'cmd1', args: [1]}, {cmd: 'cmd2', args: [2]} ]);
+		});
+		it('should not mix events from different documents', function () {
+			var idea2 = observable({});
+			mapRepository.dispatchEvent('mapLoaded', idea2, 'mapId2');
+			idea2.dispatchEvent('changed', 'cmd1', [1]);
+			mapRepository.dispatchEvent('mapLoaded', idea, 'mapId');
+			spyOn(storage, 'setItem');
+
+			idea.dispatchEvent('changed', 'cmd2', [2]);
+
+			expect(storage.setItem).toHaveBeenCalledWith('auto-save-mapId', [ {cmd: 'cmd2', args: [2]} ]);
+		});
+		it('should drop unsaved changes when a map is saved', function () {
+			expect(false).toBeTruthy();
+		});
+		it('should warn when changes could not be saved', function () {
+			expect(false).toBeTruthy();
+		});
 	});
-	it('should append multiple change events to storage', function () {
-		mapRepository.dispatchEvent('mapLoaded', idea, 'mapId');
-		spyOn(storage, 'setItem');
+	describe('applyUnsavedChanges', function () {
+		it('should apply all unsaved events to the current idea aggregate', function () {
+			var aggregate = MAPJS.content({id: 1, title: 'old'});
+			storage.setItem('auto-save-mapId', [ {cmd: 'updateTitle', args: [1, 'new title']} ]);
+			mapRepository.dispatchEvent('mapLoaded', aggregate, 'mapId');
 
-		idea.dispatchEvent('changed', 'cmd1', [1]);
-		idea.dispatchEvent('changed', 'cmd2', [2]);
+			autoSave.applyUnsavedChanges();
 
-		expect(storage.setItem).toHaveBeenCalledWith('auto-save-mapId', [ {cmd: 'cmd1', args: [1]}, {cmd: 'cmd2', args: [2]} ]);
+			expect(aggregate.title).toBe('new title');
+		});
+		it('should do nothing - but not break - if there are no unsaved changes', function () {
+			var aggregate = MAPJS.content({id: 1, title: 'old'});
+			mapRepository.dispatchEvent('mapLoaded', aggregate, 'mapId');
+
+			autoSave.applyUnsavedChanges();
+
+			expect(aggregate.title).toBe('old');
+		});
 	});
-	it('should not mix events from different documents', function () {
-    var idea2 = observable({});
-		mapRepository.dispatchEvent('mapLoaded', idea2, 'mapId2');
-		idea2.dispatchEvent('changed', 'cmd1', [1]);
-		mapRepository.dispatchEvent('mapLoaded', idea, 'mapId');
-		spyOn(storage, 'setItem');
+	describe('discardUnsavedChanges', function () {
+		it('should drop unsaved changes from storage without applying them', function () {
+			var aggregate = MAPJS.content({id: 1, title: 'old'});
+			storage.setItem('auto-save-mapId', [ {cmd: 'updateTitle', args: [1, 'new title']} ]);
+			mapRepository.dispatchEvent('mapLoaded', aggregate, 'mapId');
+			spyOn(storage, 'remove');
 
-		idea.dispatchEvent('changed', 'cmd2', [2]);
+			autoSave.discardUnsavedChanges();
 
-		expect(storage.setItem).toHaveBeenCalledWith('auto-save-mapId', [ {cmd: 'cmd2', args: [2]} ]);
+			expect(storage.remove).toHaveBeenCalledWith('auto-save-mapId');
+			expect(aggregate.title).toBe('old');
+		});
 	});
-  describe ('applyUnsavedChanges', function () {
-    it ('should apply all unsaved events to the current idea aggregate', function () {
-		  var aggregate = MAPJS.content({id: 1, title: 'old'});
-      storage.setItem('auto-save-mapId', [ {cmd: 'updateTitle', args: [1, 'new title']} ]);
-      mapRepository.dispatchEvent('mapLoaded', aggregate, 'mapId');
-
-      autoSave.applyUnsavedChanges();
-
-      expect(aggregate.title).toBe('new title');
-    });
-    it ('should do nothing - but not break - if there are no unsaved changes', function () {
-		  var aggregate = MAPJS.content({id: 1, title: 'old'});
-      mapRepository.dispatchEvent('mapLoaded', aggregate, 'mapId');
-      
-      autoSave.applyUnsavedChanges();
-
-      expect(aggregate.title).toBe('old');
-    });
-  });
-  describe ('discardUnsavedChanges', function () {
-    it ('should drop unsaved changes from storage without applying them', function () {
-		  var aggregate = MAPJS.content({id: 1, title: 'old'});
-      storage.setItem('auto-save-mapId', [ {cmd: 'updateTitle', args: [1, 'new title']} ]);
-      mapRepository.dispatchEvent('mapLoaded', aggregate, 'mapId');
-      spyOn(storage, 'remove');
-
-      autoSave.discardUnsavedChanges();
-  
-      expect(storage.remove).toHaveBeenCalledWith('auto-save-mapId');
-      expect(aggregate.title).toBe('old');
-    });
-  });
 });
