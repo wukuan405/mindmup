d179ea87882ee82302503d513eae31f1648c6fd7
Gojko Adzic <gojko@gojko.com>
2013-05-12 20:20:20 +0100

    naming cleanup, separate files for retriable decorator and file system map source

---
+++ public/retriable-map-source-decorator.js
@@ -0,0 +1,26 @@
+/*global MM, MAPJS, jQuery*/
+MM.RetriableMapSourceDecorator = function (adapter) {
+	'use strict';
+	var	shouldRetry = function (retries) {
+			var times = MM.retryTimes(retries);
+			return function (status) {
+				return times() && status === 'network-error';
+			};
+		};
+	this.loadMap = function (mapId, showAuth) {
+		return MM.retry(
+			adapter.loadMap.bind(adapter, mapId, showAuth),
+			shouldRetry(5),
+			MM.linearBackoff()
+		);
+	};
+	this.saveMap = function (contentToSave, mapId, fileName) {
+		return MM.retry(
+			adapter.saveMap.bind(adapter, contentToSave, mapId, fileName),
+			shouldRetry(5),
+			MM.linearBackoff()
+		);
+	};
+	this.description = adapter.description;
+	this.recognises = adapter.recognises;
+};
