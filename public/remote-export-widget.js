607500e896866d89bd1ca29c0ed575bcd2e6dd20
Gojko Adzic <gojko@gojko.com>
2013-05-22 12:55:41 +0100

    Revert "still not working"
    This reverts commit 207dafcf54122f358c8acac38c6911f809ae99bf.

--- public/remote-export-widget.js
+++ public/remote-export-widget.js
@@ -1,9 +1,12 @@
-/*global $, jQuery, MM,  MAPJS, window, atob, ArrayBuffer, Uint8Array, saveAs*/
+/*global $, jQuery, MM, document, MAPJS, window, atob, ArrayBuffer, Uint8Array*/
 jQuery.fn.remoteExportWidget = function (mapController, alert) {
 	'use strict';
 	var self = this,
 		loadedIdea,
 		downloadLink = $('<a>').addClass('hide').appendTo('body'),
+		joinLines = function (string) {
+			return string.replace(/\n/g, ' ').replace(/\r/g, ' ');
+		},
 		dataUriToBlob = function (dataURI) {
 			var byteString = atob(dataURI.split(',')[1]),
 				mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0],
@@ -16,17 +19,18 @@ jQuery.fn.remoteExportWidget = function (mapController, alert) {
 			return new window.Blob([ab], {type: mimeString});
 		},
 		toBlob = function (contents, mimeType) {
-			//var browserUrl = window.webkitURL || window.URL;
+			var browserUrl = window.URL || window.webkitURL;
 			if (/^data:[a-z]*\/[a-z]*/.test(contents)) {
-				return dataUriToBlob(contents);
+				return browserUrl.createObjectURL(dataUriToBlob(contents));
 			}
-			return new window.Blob([contents], {type: mimeType});
+			return browserUrl.createObjectURL(new window.Blob([contents], {type: mimeType}));
 		};
 	mapController.addEventListener('mapLoaded', function (idea) {
 		loadedIdea = idea;
 	});
-	return self.click(function () {
-		var toPromise = function (fn, mimeType) {
+	return this.click(function () {
+		var exportForm = $($(this).data('mm-target')),
+			toPromise = function (fn, mimeType) {
 				return function () {
 					return jQuery.Deferred().resolve(fn.apply(undefined, arguments), mimeType).promise();
 				};
@@ -43,6 +47,10 @@ jQuery.fn.remoteExportWidget = function (mapController, alert) {
 			elem,
 			alertId;
 		title = loadedIdea.title + '.' + format;
+		if (!downloadLink) {
+			alert.show('Unsupported browser', 'Your browser does not support HTML5 download links. Please try to export using a different browser or upgrade to the latest version', 'error');
+			return;
+		}
 		if (alert) {
 			alertId = alert.show('<i class="icon-spinner icon-spin"></i>&nbsp;Exporting map to ' + title, 'This may take a few seconds for larger maps', 'info');
 		}
@@ -57,7 +65,9 @@ jQuery.fn.remoteExportWidget = function (mapController, alert) {
 						alert.hide(alertId);
 						alertId = undefined;
 					}
-					saveAs(toBlob(contents, mimeType), title);
+					downloadLink.attr('download', title).attr('href', toBlob(contents, mimeType));
+					downloadLink[0].click();
+					return false;
 				}
 			);
 		}
