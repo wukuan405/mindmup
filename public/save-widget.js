bc209f54dfe05d5f444a5057f9042ebbe8fd0433
davedf <dave@deflorinier.me.uk>
2013-05-23 12:17:47 +0100

    block command+s for autosaved maps

--- public/save-widget.js
+++ public/save-widget.js
@@ -1,8 +1,9 @@
-/*global document, window, $, _, jQuery*/
+/*global window, $, _, jQuery*/
 jQuery.fn.saveWidget = function (mapController) {
 	'use strict';
 	var mapChanged = false,
 		repository,
+		autoSave,
 		mapChangedListener = function () {
 			mapChanged = true;
 		},
@@ -29,7 +30,9 @@ jQuery.fn.saveWidget = function (mapController) {
 		};
 	$(window).keydown(function (evt) {
 		if (evt.which === 83 && (evt.metaKey || evt.ctrlKey)) {
-			mapController.publishMap(repository);
+			if (!autoSave) {
+				mapController.publishMap(repository);
+			}
 			evt.preventDefault();
 		}
 	});
@@ -39,7 +42,8 @@ jQuery.fn.saveWidget = function (mapController) {
 	element.find('a[data-mm-repository]').addClass(function () {
 		return 'repo repo-' + $(this).data('mm-repository');
 	});
-	mapController.addEventListener('mapLoaded', function (mapId, idea) {
+	mapController.addEventListener('mapLoaded', function (mapId, idea, properties) {
+		autoSave = properties.autoSave;
 		idea.addEventListener('changed', mapChangedListener);
 	});
 	mapController.addEventListener('mapSaving', function () {
