12e0af81d87a92f6d9447bbb9436c9dc5b390a68
Gojko Adzic <gojko@gojko.com>
2013-06-25 15:38:26 +0100

    saveFile tests

--- test/e-github.js
+++ test/e-github.js
@@ -1,16 +1,16 @@
 /*global describe, it, expect, beforeEach, afterEach, MM, window, spyOn, jQuery, window, jasmine*/
-describe("Github integration", function () {
+describe('Github integration', function () {
 	'use strict';
-	describe("MM.GithubAPI", function () {
-		describe("loadFile", function () {
-			var underTest, sessionStorage, done, rejected;
-			beforeEach(function () {
-				sessionStorage = {github_auth_token: 'x'};
-				underTest = new MM.GithubAPI(sessionStorage);
-				done = jasmine.createSpy('done');
-				rejected = jasmine.createSpy('rejected');
-			});
-			it("sends a request to Github V3 API URL using auth token and base64 decodes file contents", function () {
+	describe('MM.GithubAPI', function () {
+		var underTest, sessionStorage, done, rejected;
+		beforeEach(function () {
+			sessionStorage = {github_auth_token: 'x'};
+			underTest = new MM.GithubAPI(sessionStorage);
+			done = jasmine.createSpy('done');
+			rejected = jasmine.createSpy('rejected');
+		});
+		describe('loadFile', function () {
+			it('sends a request to Github V3 API URL using auth token and base64 decodes file contents', function () {
 				spyOn(jQuery, 'ajax').andReturn(jQuery.Deferred().resolve({
 					content: window.Base64.encode('hey there'),
 					name: 'test.mup',
@@ -23,7 +23,14 @@ describe("Github integration", function () {
 				);
 				expect(rejected).not.toHaveBeenCalled();
 			});
-			it("rejects with not-authenticated if auth token is blank", function () {
+			it('attaches branch as a ?ref= to the URL if defined', function () {
+				spyOn(jQuery, 'ajax').andReturn(jQuery.Deferred().promise());
+				underTest.loadFile({repo: 'r', path: '/test.mup', branch: 'bbb'}).then(done, rejected);
+				expect(jQuery.ajax).toHaveBeenCalledWith(
+					{ url : 'https://api.github.com/repos/r/contents/test.mup?ref=bbb', type : 'GET', headers : { Authorization : 'bearer x' } }
+				);
+			});
+			it('rejects with not-authenticated if auth token is blank', function () {
 				delete sessionStorage.github_auth_token;
 				underTest.loadFile({repo: 'r', path: '/test.mup'}).then(done, rejected);
 				expect(done).not.toHaveBeenCalled();
@@ -38,6 +45,15 @@ describe("Github integration", function () {
 				underTest.loadFile({}).then(done);
 				expect(done).toHaveBeenCalledWith('hey there', 'application/x-freemind');
 			});
+			it('resolves with mime-type appliction/octet-stream for unrecognised extensions', function () {
+				spyOn(jQuery, 'ajax').andReturn(jQuery.Deferred().resolve({
+					content: window.Base64.encode('hey there'),
+					name: 'test.mxm',
+					encoding: 'base64'
+				}).promise());
+				underTest.loadFile({}).then(done);
+				expect(done).toHaveBeenCalledWith('hey there', 'application/octet-stream');
+			});
 			it('rejects with format-error if encoding is not base64', function () {
 				spyOn(jQuery, 'ajax').andReturn(jQuery.Deferred().resolve({
 					content: window.Base64.encode('hey there'),
@@ -46,6 +62,56 @@ describe("Github integration", function () {
 				}).promise());
 				underTest.loadFile({}).then(done, rejected);
 				expect(rejected).toHaveBeenCalledWith('format-error', 'Unknown encoding base65');
+				expect(done).not.toHaveBeenCalled();
+			});
+		});
+		describe('saveFile', function () {
+			beforeEach(function () {
+				spyOn(jQuery, 'ajax').andReturn(jQuery.Deferred().promise());
+			});
+			it('commits a new file without a SHA tag using PUT', function () {
+				jQuery.ajax.andReturn(jQuery.Deferred().reject().promise());
+				underTest.saveFile('abcd', {repo: 'r', path: 'p.txt'}, 'commit msg');
+				expect(jQuery.ajax).toHaveBeenCalledWith({
+					url: 'https://api.github.com/repos/r/contents/p.txt',
+					type : 'PUT',
+					headers : { Authorization : 'bearer x' },
+					data : '{"content":"' + window.Base64.encode('abcd') + '","message":"commit msg","sha":""}',
+					processData : false
+				});
+			});
+			it('commits an existing file with the previous sha tag SHA tag using PUT', function () {
+				jQuery.ajax.andReturn(jQuery.Deferred().resolve({sha: 'oldsha'}).promise());
+				underTest.saveFile('abcd', {repo: 'r', path: 'p.txt'}, 'commit msg');
+				expect(jQuery.ajax).toHaveBeenCalledWith({
+					url: 'https://api.github.com/repos/r/contents/p.txt',
+					type : 'PUT',
+					headers : { Authorization : 'bearer x' },
+					data : '{"content":"' + window.Base64.encode('abcd') + '","message":"commit msg","sha":"oldsha"}',
+					processData : false
+				});
+			});
+			it('attaches the branch to PUT request as a branch data arg if defined', function () {
+				jQuery.ajax.andReturn(jQuery.Deferred().resolve({sha: 'oldsha'}).promise());
+				underTest.saveFile('abcd', {repo: 'r', branch: 'someb', path: 'p.txt'}, 'commit msg');
+				expect(jQuery.ajax).toHaveBeenCalledWith({
+					url: 'https://api.github.com/repos/r/contents/p.txt',
+					type : 'PUT',
+					headers : { Authorization : 'bearer x' },
+					data : '{"content":"' + window.Base64.encode('abcd') + '","message":"commit msg","sha":"oldsha","branch":"someb"}',
+					processData : false
+				});
+			});
+			it('resolves successful completion', function () {
+				jQuery.ajax.andReturn(jQuery.Deferred().resolve({sha: 'oldsha'}).promise());
+				underTest.saveFile('abcd', {repo: 'r', branch: 'someb', path: 'p.txt'}, 'commit msg').then(done, rejected);
+				expect(done).toHaveBeenCalled();
+				expect(rejected).not.toHaveBeenCalled();
+			});
+			it('rejects unsuccessful completion', function () {
+				jQuery.ajax.andReturn(jQuery.Deferred().reject().promise());
+				underTest.saveFile('abcd', {repo: 'r', branch: 'someb', path: 'p.txt'}, 'commit msg').then(done, rejected);
+				expect(rejected).toHaveBeenCalled();
 				expect(done).not.toHaveBeenCalled();
 			});
 		});
