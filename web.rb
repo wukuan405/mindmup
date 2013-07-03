171f9a5fa6aaad638e26283ca8a9b887b41c0fdc
davedf <dave@deflorinier.me.uk>
2013-07-03 13:23:16 +0100

    change to not redirecting wiht unsupported browser, do no-cache instead

--- web.rb
+++ web.rb
@@ -164,7 +164,10 @@ helpers do
     if (browser_supported? || user_accepted_browser?)
       erb :editor
     else
-      redirect '/un'
+      response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+      response.headers['Pragma'] = 'no-cache'
+      response.headers['Expires'] = '0'
+      erb :unsupported
     end
   end
   def user_accepted_browser?
