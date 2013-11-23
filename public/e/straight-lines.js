5fbf35f5db432466f55e1a8b14159746ac7bc074
David de Florinier <dave@deflorinier.me.uk>
2013-11-23 09:03:44 +0000

    Revert "Merge branch 'master' of heroku.com:evening-tundra-5858"
    This reverts commit 74e95252facbe1962045d58448170e3a7da81357, reversing
    changes made to 25938828ba84b887f1daa6eee329bc9a6d1e7b69.

--- public/e/straight-lines.js
+++ public/e/straight-lines.js
@@ -1,7 +1,8 @@
 /*global Kinetic*/
-Kinetic.Connector.prototype.drawFunc = function (context) {
+Kinetic.Connector.prototype.drawFunc = function (canvas) {
 	'use strict';
-	var shapeFrom = this.shapeFrom,
+	var context = canvas.getContext(),
+		shapeFrom = this.shapeFrom,
 		shapeTo = this.shapeTo;
 	if (!this.isVisible()) {
 		return;
@@ -9,5 +10,5 @@ Kinetic.Connector.prototype.drawFunc = function (context) {
 	context.beginPath();
 	context.moveTo(shapeFrom.getX() + shapeFrom.getWidth() / 2, shapeFrom.getY() + shapeFrom.getHeight() / 2);
 	context.lineTo(shapeTo.getX() + shapeTo.getWidth() / 2, shapeTo.getY() + shapeTo.getHeight() / 2);
-	context.stroke(this);
+	canvas.stroke(this);
 };
