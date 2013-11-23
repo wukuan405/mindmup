8a3a627004b39c26e6c46062c1e3e7c94b40c590
Gojko Adzic <gojko@gojko.com>
2013-11-23 20:49:26 +0100

    reverted link changes, to make maps work again

--- public/mapjs-compiled.js
+++ public/mapjs-compiled.js
@@ -2337,8 +2337,9 @@ MAPJS.dragdrop = function (mapModel, stage) {
 				child.getX(), child.getY(), child.getWidth(), child.getHeight());
 		};
 	Kinetic.Link.prototype = {
-		drawHitFunc: function (context) {
-			var shapeFrom = this.shapeFrom,
+		drawHitFunc: function (canvas) {
+			var context = canvas.getContext(),
+				shapeFrom = this.shapeFrom,
 				shapeTo = this.shapeTo,
 				conn,
 				strokeWidth = this.getStrokeWidth();
@@ -2348,11 +2349,12 @@ MAPJS.dragdrop = function (mapModel, stage) {
 			context.beginPath();
 			context.moveTo(conn.from.x, conn.from.y);
 			context.lineTo(conn.to.x, conn.to.y);
-			context.fillStrokeShape(this);
+			canvas.stroke(this);
 			this.setStrokeWidth(strokeWidth);
 		},
-		drawFunc: function (context) {
-			var shapeFrom = this.shapeFrom,
+		drawFunc: function (canvas) {
+			var context = canvas.getContext(),
+				shapeFrom = this.shapeFrom,
 				shapeTo = this.shapeTo,
 				conn,
 				n = Math.tan(Math.PI / 9);
@@ -2361,7 +2363,7 @@ MAPJS.dragdrop = function (mapModel, stage) {
 			context.beginPath();
 			context.moveTo(conn.from.x, conn.from.y);
 			context.lineTo(conn.to.x, conn.to.y);
-			context.fillStrokeShape(this);
+			canvas.stroke(this);
 			if (this.attrs.arrow) {
 				var a1x, a1y, a2x, a2y, len = 14, iy, m,
 					dx = conn.to.x - conn.from.x,
@@ -2386,7 +2388,7 @@ MAPJS.dragdrop = function (mapModel, stage) {
 				context.lineTo(conn.to.x, conn.to.y);
 				context.lineTo(a2x, a2y);
 				context.lineTo(a1x, a1y);
-				context.fillStrokeShape(this);
+				context.fill();
 			}
 		}
 	};
@@ -2400,7 +2402,6 @@ Kinetic.Link.prototype.setMMAttr = function (newMMAttr) {
 			dashed: [8, 8]
 		};
 	this.setStroke(style && style.color || 'red');
-	this.setFill(style.color || 'red');
 	this.setDashArray(dashTypes[style && style.lineStyle || 'dashed']);
 	this.attrs.arrow = style && style.arrow || false;
 };
