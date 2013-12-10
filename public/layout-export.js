ab551099dadb6f4744a223fed08919a3e956c9d2
David de Florinier <dave@deflorinier.me.uk>
2013-12-10 14:33:19 +0000

    move pdf Export to it's own ui fragment

--- public/layout-export.js
+++ public/layout-export.js
@@ -24,29 +24,34 @@ MM.LayoutExportController = function (mapModel, fileSystem, resultPoller, activi
 
 jQuery.fn.layoutExportWidget = function (layoutExportController, modalConfirmation, alert) {
 	'use strict';
-	var alertId;
-	this.click(function () {
-		var exportComplete = function (url) {
-				alert.hide(alertId);
-				alertId = alert.show('PDF export complete: ', '<a target="_blank" href="' + url + '">Click here to open</a> (This link will remain available for 24 hours)');
+	var alertId,
+		self = this,
+		format = self.data('mm-format'),
+		confirmElement = self.find('[data-mm-role=export]'),
+		exportComplete = function (url) {
+			alert.hide(alertId);
+			alertId = alert.show(format + ' export complete: ', '<a target="_blank" href="' + url + '">Click here to open</a> (This link will remain available for 24 hours)');
+		},
+		exportFailed = function (reason) {
+			var reasonMap = {
+				'file-too-large': 'Your map is too large',
+				'polling-timeout': 'The server is too busy. Please try again later, or if this error persists contact us at <a href="mailto:contact@mindmup.com?subject=MindMup%20PDF%20Export">contact@mindmup.com</a>'
 			},
-			exportFailed = function (reason) {
-				var reasonMap = {
-					'file-too-large': 'Your map is too large',
-					'polling-timeout': 'The server is too busy. Please try again later, or if this error persists contact us at <a href="mailto:contact@mindmup.com?subject=MindMup%20PDF%20Export">contact@mindmup.com</a>'
-				},
-				reasonDescription = reasonMap[reason] || reason;
-				alert.hide(alertId);
+			reasonDescription = reasonMap[reason] || reason;
+			alert.hide(alertId);
 
-				alertId = alert.show('Unable to export PDF: ', reasonDescription, 'error');
-			},
-			doExport = function () {
-				alert.hide(alertId);
-				alertId = alert.show('<i class="icon-spinner icon-spin"></i>&nbsp;Please wait, exporting the map...');
-				layoutExportController.startExport({'export': {'page-size': 'A4', 'orientation': 'landscape'}}).then(exportComplete, exportFailed);
-			};
-		alert.hide(alertId);
-		modalConfirmation.showModalToConfirm('Export to PDF (Beta)', 'Exporting maps to PDF is a work in progress at the moment. In this beta version we only support exporting maps up to 100k and the generated PDF will be saved in a <strong>publicly accessible location</strong>.', 'Proceed').then(doExport);
+			alertId = alert.show('Unable to export ' + format + ': ', reasonDescription, 'error');
+		},
+		doExport = function () {
+			alert.hide(alertId);
+			alertId = alert.show('<i class="icon-spinner icon-spin"></i>&nbsp;Please wait, exporting the map...');
+			layoutExportController.startExport({'export': {'page-size': 'A4', 'orientation': 'landscape'}}).then(exportComplete, exportFailed);
+			self.modal('hide');
+		};
+	confirmElement.click(doExport).keydown('space', doExport);
+	self.modal({keyboard: true, show: false});
+	this.on('shown', function () {
+		confirmElement.focus();
 	});
-	//
+
 };
