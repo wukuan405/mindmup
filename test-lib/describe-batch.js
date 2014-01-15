4cc51cda529b20cbab69f5649730167bcd9775a8
David de Florinier <dave@deflorinier.me.uk>
2014-01-15 13:33:40 +0000

    added extra implementation of describe batch

--- test-lib/describe-batch.js
+++ test-lib/describe-batch.js
@@ -1,4 +1,4 @@
-/*global describe, it, */
+/*global describe, it, _, expect*/
 (function describeBatch() {
 	'use strict';
 	var oldDescribe = describe;
@@ -8,16 +8,35 @@
 			return oldDescribe.apply(this, arguments);
 		}
 		optionalBatch = arguments[1];
-		if (arguments.length === 3 && Array.isArray(optionalBatch)) {
+		if (arguments.length === 3) {
 			parameterizedSpec = arguments[2];
-			oldDescribe.call(this, arguments[0], function () {
-				optionalBatch.forEach(function(args) {
-					var specArgs = args.slice(1);
-					it.call(this, args[0], function () {
-						parameterizedSpec.apply(this, specArgs);
+			if (Array.isArray(optionalBatch)) {
+				oldDescribe.call(this, arguments[0], function () {
+					optionalBatch.forEach(function (args) {
+						var specArgs = args.slice(1);
+						it.call(this, args[0], function () {
+							parameterizedSpec.apply(this, specArgs);
+						});
 					});
 				});
-			});
+			}  else {
+				oldDescribe.call(this, arguments[0], function () {
+					_.each(optionalBatch, function (specArgs, desc) {
+						it.call(this, desc, function () {
+							parameterizedSpec.apply(this, specArgs);
+						});
+					});
+				});
+			}
 		}
 	};
 }());
+describe('batch describe', function () {
+	'use strict';
+	describe('cases as object properties', {
+		'first': [1, 2],
+		'second': [2, 4]
+	}, function (num, twice) {
+		expect(twice).toEqual(num * 2);
+	});
+});
