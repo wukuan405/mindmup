6825f16b5570c0110ce9d74861a2b9f6c8cadae5
David de Florinier <dave@deflorinier.me.uk>
2014-02-09 09:50:37 +0000

    download dependencies for offline testing

--- GruntFile.js
+++ GruntFile.js
@@ -67,11 +67,11 @@ module.exports = function (grunt) {
 						'test/*.js',
 					],
 					vendor: [
-						'http://mindmup.s3.amazonaws.com/lib/jquery-1.9.1.min.js',
-						'http://mindmup.s3.amazonaws.com/lib/bootstrap-2.3.1.min.js',
-						'http://mindmup.s3.amazonaws.com/lib/jquery-ui-1.10.0.custom.min.js',
-						'http://mindmup.s3.amazonaws.com/lib/kinetic-v4.5.4.min.js',
-						'http://mindmup.s3.amazonaws.com/lib/color-0.4.1.min.js'
+						'downloads/jquery-1.9.1.min.js',
+						'downloads/bootstrap-2.3.1.min.js',
+						'downloads/jquery-ui-1.10.0.custom.min.js',
+						'downloads/kinetic-v4.5.4.min.js',
+						'downloads/color-0.4.1.min.js'
 					],
 					helpers: [
 						'test-lib/mm.js',
---
+++ dl_dependencies.sh
@@ -0,0 +1,10 @@
+rm -rf downloads
+
+mkdir -p downloads
+
+curl  http://mindmup.s3.amazonaws.com/lib/jquery-1.9.1.min.js > downloads/jquery-1.9.1.min.js
+curl  http://mindmup.s3.amazonaws.com/lib/bootstrap-2.3.1.min.js > downloads/bootstrap-2.3.1.min.js
+curl  http://mindmup.s3.amazonaws.com/lib/jquery-ui-1.10.0.custom.min.js > downloads/jquery-ui-1.10.0.custom.min.js
+curl  http://mindmup.s3.amazonaws.com/lib/kinetic-v4.5.4.min.js > downloads/kinetic-v4.5.4.min.js
+curl  http://mindmup.s3.amazonaws.com/lib/color-0.4.1.min.js > downloads/color-0.4.1.min.js
+curl http://mindmup.s3.amazonaws.com/lib/underscore-1.4.4.min.js > downloads/underscore-1.4.4.min.js
