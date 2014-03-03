8eddda3f00384d91bbac5d87d8638c97ebe05922
Gojko Adzic <gojko@gojko.com>
2014-03-03 16:36:15 +0000

    css using ssl-compatible cdn url

--- compiled_ts.rb
+++ compiled_ts.rb
@@ -1,1 +1,1 @@
-set :compiled_ts,"20140303161655"
+set :compiled_ts,"20140303163519"
--- upload.sh
+++ upload.sh
@@ -6,6 +6,7 @@ cat `grep 'src="/' views/embedded_scripts.erb | sed 's/.*"\/\([^"]*\)".*/public\
 mkdir compiled/e
 cp public/e/* compiled/e/
 grunt compile
+gsed s/static.mindmup.com/d23c2zpg6dm0n.cloudfront.net/g compiled/mindmap.css -i
 rc=$?
 if [[ $rc != 0 ]] ; then
   echo "grunt failed, bailing out"
