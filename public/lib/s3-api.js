51b2be4c711bf55d6a2c23848473bae2f9f15dc9
David de Florinier <dave@deflorinier.me.uk>
2014-03-12 20:38:25 +0000

    pdf timeout -> 30 seconds

--- public/lib/s3-api.js
+++ public/lib/s3-api.js
@@ -46,7 +46,7 @@ MM.S3Api = function () {
 		}).then(deferred.resolve, saveFailed);
 		return deferred.promise();
 	};
-	self.pollerDefaults = {sleepPeriod: 1000, timeoutPeriod: 20000};
+	self.pollerDefaults = {sleepPeriod: 1000, timeoutPeriod: 30000};
 	self.poll = function (signedListUrl, options) {
 		var sleepTimeoutId,
 			timeoutId,
