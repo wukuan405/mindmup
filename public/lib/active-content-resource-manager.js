23ca5166e53cea0352e2c86a3bbe0cb3b25e2cf9
Gojko Adzic <gojko@gojko.com>
2014-06-16 18:48:57 +0200

    lint

--- public/lib/active-content-resource-manager.js
+++ public/lib/active-content-resource-manager.js
@@ -1,6 +1,5 @@
 /*global MM */
 /* todo:
- * - protect against false cache hits when a map is reloaded (eg instert an index of map loaded in front of every resource)
  * - collab maps - transfer resources
  */
 MM.ActiveContentResourceManager = function (activeContentListener, prefixTemplate) {
@@ -10,8 +9,9 @@ MM.ActiveContentResourceManager = function (activeContentListener, prefixTemplat
 		prefixMatcher = new RegExp('^' + prefix),
 		folder,
 		buildUniqueString = function () {
-			return 'xxxxxxxx-yxxx-yxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-				var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			/*jshint bitwise: false*/
+			return 'xxxxxxxx-yxxx-yxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+				var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r&0x3|0x8);
 				return v.toString(16);
 			});
 		};
