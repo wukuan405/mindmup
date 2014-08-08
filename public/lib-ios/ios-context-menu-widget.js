09058810a0e9540d48991f44e4ba5a4846993baf
David de Florinier <dave@deflorinier.me.uk>
2014-08-08 12:04:18 +0100

    extract popover functionality to separate widget

--- public/lib-ios/ios-context-menu-widget.js
+++ public/lib-ios/ios-context-menu-widget.js
@@ -1,21 +1,10 @@
-/*global jQuery, window */
+/*global jQuery */
 jQuery.fn.iosContextMenuWidget = function (mapModel, tools) {
 	'use strict';
 	return jQuery(this).each(function () {
 		var element = jQuery(this),
 				toolbar = element.find('[data-mm-role~="context-menu-toolbar"]'),
-				toolContainer = element.find('[data-mm-role~="context-menu-tool-container"]'),
-				topPointer = element.find('[data-mm-role="toolbar-pointer-top"]'),
-				bottomPointer = element.find('[data-mm-role="toolbar-pointer-bottom"]'),
-				hideToolbar = function () {
-					if (element.is(':visible')) {
-						element.unbind('click');
-						element.hide();
-					}
-				},
-				backgroundClick = function () {
-					hideToolbar();
-				};
+				toolContainer = element.find('[data-mm-role~="context-menu-tool-container"]');
 		toolbar.click(function (e) {
 			e.preventDefault();
 			e.stopPropagation();
@@ -23,38 +12,12 @@ jQuery.fn.iosContextMenuWidget = function (mapModel, tools) {
 		if (tools) {
 			tools.each(function () {
 				jQuery(this).clone(true).css('display', '').appendTo(toolContainer).click(function () {
-					hideToolbar();
+					element.trigger(jQuery.Event('hidePopover'));
 				});
 			});
 		}
 		mapModel.addEventListener('contextMenuRequested', function (nodeId, x, y) {
-			var topBottomHeight = (element.height() -  toolbar.outerHeight() + 20),
-					showAbove = (y > topBottomHeight),
-					maxLeft = element.width() - toolbar.outerWidth() - 10,
-					minLeft = 10,
-					left = Math.max(minLeft, Math.min(maxLeft, x - (toolbar.outerWidth() / 2))),
-					top = showAbove ? (y - toolbar.outerHeight() - 10) : y + 10,
-					pointerMinLeft = 20,
-					pointerMaxLeft = toolbar.outerWidth() - 20,
-					pointerLeft = Math.max(pointerMinLeft, Math.min(pointerMaxLeft, (x - left - 10)));
-			if (showAbove) {
-				bottomPointer.css('left', pointerLeft + 'px');
-				topPointer.hide();
-				bottomPointer.show();
-			} else {
-				topPointer.show();
-				bottomPointer.hide();
-				topPointer.css('left', pointerLeft + 'px');
-			}
-			toolbar.css({'top': top + 'px', 'left': left + 'px'});
-			//stop the click handler being added to soon or it fires immediately
-			window.setTimeout(function () {
-				if (element.is(':visible')) {
-					element.click(backgroundClick);
-				}
-			}, 1000);
-
-			element.show();
+			element.trigger(jQuery.Event('showPopover', {'x': x, 'y': y}));
 		});
 	});
 };
---
+++ public/lib-ios/ios-popover-menu-widget.js
@@ -0,0 +1,56 @@
+/*global jQuery, window */
+jQuery.fn.iosPopoverMenuWidget = function () {
+	'use strict';
+	return jQuery(this).each(function () {
+		var element = jQuery(this),
+				toolbar = element.find('[data-mm-role~="context-menu-toolbar"]'),
+				topPointer = element.find('[data-mm-role="toolbar-pointer-top"]'),
+				bottomPointer = element.find('[data-mm-role="toolbar-pointer-bottom"]'),
+				hideToolbar = function () {
+					if (element.is(':visible')) {
+						element.unbind('click');
+						element.hide();
+					}
+				},
+				backgroundClick = function () {
+					hideToolbar();
+				};
+		toolbar.click(function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+		});
+		element.on('hidePopover', function () {
+			hideToolbar();
+		});
+		element.on('showPopover', function (evt) {
+			var x = evt.x,
+					y = evt.y,
+					topBottomHeight = (element.height() -  toolbar.outerHeight() + 20),
+					showAbove = (y > topBottomHeight),
+					maxLeft = element.width() - toolbar.outerWidth() - 10,
+					minLeft = 10,
+					left = Math.max(minLeft, Math.min(maxLeft, x - (toolbar.outerWidth() / 2))),
+					top = showAbove ? (y - toolbar.outerHeight() - 10) : y + 10,
+					pointerMinLeft = 20,
+					pointerMaxLeft = toolbar.outerWidth() - 20,
+					pointerLeft = Math.max(pointerMinLeft, Math.min(pointerMaxLeft, (x - left - 10)));
+			if (showAbove) {
+				bottomPointer.css('left', pointerLeft + 'px');
+				topPointer.hide();
+				bottomPointer.show();
+			} else {
+				topPointer.show();
+				bottomPointer.hide();
+				topPointer.css('left', pointerLeft + 'px');
+			}
+			toolbar.css({'top': top + 'px', 'left': left + 'px'});
+			//stop the click handler being added to soon or it fires immediately
+			window.setTimeout(function () {
+				if (element.is(':visible')) {
+					element.click(backgroundClick);
+				}
+			}, 1000);
+			element.show();
+		});
+	});
+};
