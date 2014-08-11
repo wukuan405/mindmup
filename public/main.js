ae4a666a794a7c5f358be9a20478f430fe7c0dae
Gojko Adzic <gojko@gojko.com>
2014-08-11 18:26:39 +0200

    0.0.0.3 chrome app

--- public/main.js
+++ public/main.js
@@ -18,7 +18,7 @@ MM.main = function (config) {
 				removeItem: function (key) { delete this[key]; }
 			};
 		},
-		browserStorage = getStorage(),
+		browserStorage = config.storage || getStorage(),
 		mapModelAnalytics = false,
 		setupTracking = function (activityLog, jotForm, mapModel) {
 			activityLog.addEventListener('log', function () { _gaq.push(['_trackEvent'].concat(Array.prototype.slice.call(arguments, 0, 3))); });
