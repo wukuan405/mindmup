a42eafd15e9e0fc8f3561c0e8e77dd13a941026e
David de Florinier <dave@deflorinier.me.uk>
2014-09-12 19:30:00 +0100

    updated to latest content dir

--- web.rb
+++ web.rb
@@ -20,7 +20,7 @@ def cache_last_news
   end
 end
 configure do
-  static_ts = '20140610131027'
+  static_ts = '20140912202404'
   public_host = ENV['PUBLIC_HOST'] || 'http://static.mindmup.net'
   set :static_host, "#{public_host}/#{static_ts}"
   set :static_image_host, "#{public_host}/img"
