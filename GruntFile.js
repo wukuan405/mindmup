3198f8b1797ee3afc75287bc462c956a3bf002ef
Gojko Adzic <gojko@gojko.com>
2015-01-06 14:28:01 +0100

    jq upgrade

--- GruntFile.js
+++ GruntFile.js
@@ -103,7 +103,7 @@ module.exports = function (grunt) {
 						'test/*.js',
 					],
 					vendor: [
-						grunt.option('external-scripts') || 'http://d1g6a398qq2djm.cloudfront.net/20141230102849/external.js'
+						grunt.option('external-scripts') || 'http://d1g6a398qq2djm.cloudfront.net/20150106142106/external.js'
 					],
 					helpers: [
 						'test-lib/sinon-1.5.2.js',
