1413debfdfaaf624ec10a2cc8c3b267fafabf4e4
David de Florinier <dave@deflorinier.me.uk>
2015-01-28 15:26:50 +0000

    fix jscs warnings in background-upload-widget

--- public/lib/background-upload-widget.js
+++ public/lib/background-upload-widget.js
@@ -4,9 +4,15 @@ $.fn.background_upload = function (action, start, complete, fail) {
 	var element = this,
 		sequence = $('iframe').length,
 		active = false;
-	start = start || function (name) { console.log('Uploading', name); };
-	complete = complete || function (content) { console.log('Uploaded', content); };
-	fail = fail || function (error) { console.log('Upload error', error); };
+	start = start || function (name) {
+		console.log('Uploading', name);
+	};
+	complete = complete || function (content) {
+		console.log('Uploaded', content);
+	};
+	fail = fail || function (error) {
+		console.log('Upload error', error);
+	};
 	$('<iframe style="display:none" name="upload-' + sequence + '"></iframe>').appendTo('body').load(
 		function () {
 			var result, fileType = active;
