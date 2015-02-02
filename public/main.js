a676e019f661f4939402dc3868daf8bdc89ebd81
David de Florinier <dave@deflorinier.me.uk>
2015-02-02 14:59:45 +0000

    replace modal collaborator list with a floating toolbar, unstyled atm

--- public/main.js
+++ public/main.js
@@ -137,6 +137,7 @@ MM.main = function (config) {
 				};
 				jQuery('#toolbarEdit').mapToolbarWidget(mapModel);
 				jQuery('#floating-toolbar').floatingToolbarWidget();
+				jQuery('#floating-collaborators').floatingToolbarWidget();
 				jQuery('#listBookmarks').bookmarkWidget(mapBookmarks, alert, mapController);
 				jQuery(document).titleUpdateWidget(mapController);
 
@@ -188,7 +189,7 @@ MM.main = function (config) {
 				jQuery('#customStyleModal').customStyleWidget(customStyleController);
 				jQuery('[data-mm-role~=new-map]').newMapWidget(mapController);
 				jQuery('#container').collaboratorPhotoWidget(collaborationModel, MM.deferredImageLoader, 'mm-collaborator');
-				jQuery('#modalCollaboratorList').collaboratorListWidget(collaborationModel, 'mm-has-collaborators');
+				jQuery('#floating-collaborators').collaboratorListWidget(collaborationModel, 'mm-has-collaborators');
 				jQuery('.modal').modalLauncherWidget(mapModel);
 				jQuery('input[data-mm-role~=selectable-read-only]').selectableReadOnlyInputWidget();
 				jQuery('textarea[data-mm-role~=selectable-read-only]').selectableReadOnlyInputWidget();
