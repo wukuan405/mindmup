a50f7ea0b97b4e58755053c06ef6aabd1b7fdaaf
Gojko Adzic <gojko@gojko.com>
2015-02-04 13:17:51 +0100

    moved joined/left alerts to the speech bubble

--- public/lib/collaborator-speech-bubble-widget.js
+++ public/lib/collaborator-speech-bubble-widget.js
@@ -11,37 +11,55 @@ jQuery.fn.collaboratorSpeechBubbleWidget = function (collaborationModel) {
 			img = element.find('[data-mm-role=collaborator-photo]'),
 			contentTemplate = element.find('[data-mm-role=popover-content-template]').detach(),
 			titleTemplate = element.find('[data-mm-role=popover-title-template]').detach(),
-			popoverContent = function (nodeTitle) {
-				contentTemplate.find('[data-mm-role=popover-content]').text(nodeTitle);
-				return contentTemplate.html();
+			popoverContent = function (message, style) {
+				var template = contentTemplate.clone();
+				template.find('[data-mm-role=popover-content]').text(message);
+				if (style) {
+					template.find('[data-mm-role=popover-content]').addClass(style);
+				}
+				return template.html();
 			},
 			popoverTitle = function (nodeTitle) {
 				titleTemplate.find('[data-mm-role=popover-title]').text(nodeTitle);
 				return titleTemplate.html();
 			},
+			showSpeechBubble = _.throttle(function (collaborator, message, style) {
+					currentCollaborator = collaborator;
+					img.popover('destroy');
+					img.attr('src', collaborator.photoUrl);
+					img.css('border-color', collaborator.color);
+					img.popover({
+						title: popoverTitle(collaborator.name),
+						content: popoverContent(message, style),
+						placement: 'right',
+						trigger: 'manual',
+						animation: true,
+						html: true
+					});
+					element.fadeIn(200, function () {
+						setTimeout(function () {
+							img.popover('destroy');
+							element.fadeOut();
+						}, timeout);
+					});
+					img.popover('show');
+				}, timeout + 700, {trailing: false}),
 			onEdit = function (collaborator, node) {
-				currentCollaborator = collaborator;
-				img.popover('destroy');
-				img.attr('src', collaborator.photoUrl);
-				img.css('border-color', collaborator.color);
-				img.popover({
-					title: popoverTitle(collaborator.name),
-					content: popoverContent(node.title),
-					placement: 'right',
-					trigger: 'manual',
-					animation: true,
-					html: true
-				});
-				element.fadeIn(200, function () {
-					setTimeout(function () {
-						img.popover('destroy');
-						element.fadeOut();
-					}, timeout);
-				});
-				img.popover('show');
+				var trimmedTitle = node.title && node.title.trim(),
+						style = trimmedTitle ? '' : 'muted',
+						nodeTitle = trimmedTitle || 'removed node content';
+				showSpeechBubble(collaborator, nodeTitle, style);
+			},
+			onJoin = function (collaborator) {
+				showSpeechBubble(collaborator, 'joined the session', 'muted');
+			},
+			onLeave = function (collaborator) {
+				showSpeechBubble(collaborator, 'left the session', 'muted');
 			};
 		img.click(showCollaborator);
-		collaborationModel.addEventListener('collaboratorDidEdit', _.throttle(onEdit, timeout + 700, {trailing: false}));
+		collaborationModel.addEventListener('collaboratorDidEdit', onEdit);
+		collaborationModel.addEventListener('collaboratorJoined', onJoin);
+		collaborationModel.addEventListener('collaboratorLeft', onLeave);
 	});
 };
 
