e0d922c47034c63a069760d749f12a0a88618e2a
David de Florinier <dave@deflorinier.me.uk>
2015-03-31 14:40:04 +0100

    send results back to native code

--- public/lib-ios/4/ios-export-request-handler.js
+++ public/lib-ios/4/ios-export-request-handler.js
@@ -1,6 +1,6 @@
-/*global MM, observable*/
+/*global MM, observable, jQuery*/
 
-MM.IOS.ExportRequestHandler = function (serverConnector, activityLog, activeContentListener, mmProxy) {
+MM.IOS.ExportRequestHandler = function (serverConnector, activityLog, activeContentListener, mmProxy, resultContainerSelector) {
 	'use strict';
 	var self = observable(this),
 		sharePostProcessing = MM.buildDecoratedResultProcessor(MM.ajaxResultProcessor, MM.layoutExportDecorators),
@@ -11,6 +11,7 @@ MM.IOS.ExportRequestHandler = function (serverConnector, activityLog, activeCont
 			return new MM.LayoutExportController(exportHandlers, serverConnector.goldApi, serverConnector.s3Api, activityLog);
 		},
 		layoutExportController = buildLayoutExportController();
+	resultContainerSelector = resultContainerSelector || 'ios-export-result';
 	self.handlesCommand = function (command) {
 		return command && command.type && command.type == 'exportRequest';
 	};
@@ -18,9 +19,11 @@ MM.IOS.ExportRequestHandler = function (serverConnector, activityLog, activeCont
 		var format = command && command.args && command.args[0];
 		if (format) {
 			self.dispatchEvent('exportRequest', format, function (widgetElement) {
+				var resultContainer = widgetElement.find('[data-mm-role~="' + resultContainerSelector + '"]');
 				widgetElement.iosModalWidget(mmProxy);
 				widgetElement.layoutExportWidget(layoutExportController);
 				widgetElement.atlasPrepopulationWidget(activeContentListener, 40, 150);
+				widgetElement.find('[data-mm-role~="ios-send-export"]').iosExportResultSendWidget(mmProxy, resultContainer);
 			});
 		}
 	};
@@ -29,4 +32,31 @@ MM.IOS.ExportRequestHandler = function (serverConnector, activityLog, activeCont
 		layoutExportController = buildLayoutExportController();
 	});
 
+};
+
+MM.layoutExportDecorators.iosResultDecorator = function (exportResult) {
+	'use strict';
+	var iosResult = {
+		'thumb-png': exportResult['thumb-png'],
+		'index-html': exportResult['index-html'],
+		'archive-zip': exportResult['archive-zip'],
+		'export': exportResult['export']
+	};
+	exportResult['ios-export-result'] = JSON.stringify(iosResult);
+};
+
+jQuery.fn.iosExportResultSendWidget = function (mmProxy, resultContainer) {
+	'use strict';
+	return jQuery(this).each(function () {
+		var element = jQuery(this);
+		element.click(function () {
+			var result = resultContainer && resultContainer.size && resultContainer.size() > 0 && resultContainer.val(),
+				sendMethod = element && result && result !== '' && element.data('mm-method'),
+				resultObj = sendMethod && result && JSON.parse(result);
+			if (resultObj) {
+				resultObj.sendMethod = sendMethod;
+				mmProxy.sendMessage({type: 'sendExportResult', args: resultObj});
+			}
+		});
+	});
 };
